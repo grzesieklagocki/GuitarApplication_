ab9e37062e30ac1f3e891ff519b1cce39e5f28fb
grzesieklagocki <grzesieklagocki@gmail.com>
2016-06-04 21:21:53 +0200

    pierwsza alpha

---
+++ GuitarApplication/GUIMethods.cs
@@ -0,0 +1,66 @@
+﻿using Sounds;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace GuitarApplication
+{
+    public partial class MainWindow : Window
+    {
+        private void InitFretLabels(bool hintEnable)
+        {
+            for (int x = 0; x < 13; x++)
+                for (int y = 0; y < 6; y++)
+                {
+                    var label = new Label();
+
+                    label.SetValue(Grid.ColumnProperty, x);
+                    label.SetValue(Grid.RowProperty, y);
+                    label.SetValue(Label.ForegroundProperty, Brushes.White);
+
+                    if (hintEnable)
+                        label.SetValue(Label.ContentProperty, Sound.GetSoundName(y, x));
+
+                    label.SetValue(Label.MarginProperty, new Thickness(1));
+                    label.SetValue(Label.BackgroundProperty, GetGradient(Colors.White));
+                    label.SetValue(Label.OpacityProperty, 0.0);
+
+                    label.MouseDown += new MouseButtonEventHandler(Cell_MouseDown);
+
+                    GridFretoboard.Children.Add(label);
+                }
+        }
+
+        private LinearGradientBrush GetGradient(Color centerColor)
+        {
+            LinearGradientBrush gradient = new LinearGradientBrush();
+            gradient.StartPoint = new Point(0, 0);
+            gradient.EndPoint = new Point(0, 1);
+
+            GradientStop start = new GradientStop();
+            start.Color = Colors.Black;
+            start.Offset = 0.0;
+            gradient.GradientStops.Add(start);
+
+            GradientStop center = new GradientStop();
+            center.Color = centerColor;
+            center.Offset = 0.5;
+            gradient.GradientStops.Add(center);
+
+            GradientStop stop = new GradientStop();
+            stop.Color = Colors.Black;
+            stop.Offset = 1.0;
+            gradient.GradientStops.Add(stop);
+
+            return gradient;
+        }
+
+        private static void RandomizeSound()
+        {
+            Sound.Randomize();
+            Sound.Play();
+        }
+    }
+
+}
--- GuitarApplication/MainWindow.xaml.cs
+++ GuitarApplication/MainWindow.xaml.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Sounds;
+using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Media.Animation;
 
 namespace GuitarApplication
 {
@@ -20,9 +13,65 @@ namespace GuitarApplication
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DoubleAnimation animation;
+
         public MainWindow()
         {
+            animation = new DoubleAnimation
+            {
+                To = 0,
+                BeginTime = TimeSpan.FromSeconds(0),
+                Duration = TimeSpan.FromSeconds(3),
+                FillBehavior = FillBehavior.HoldEnd
+            };
+
             InitializeComponent();
+
+            InitFretLabels(true);
+
+            Sound.Randomize();
+        }
+
+        private void Cell_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+            if (sender != null)
+            {
+                var label = sender as Label;
+
+                int row = (int)label.GetValue(Grid.RowProperty);
+                int column = (int)label.GetValue(Grid.ColumnProperty);
+
+                Sounds.Sound.Play(5 - row, column);
+                
+                if (!btnRandomize.IsEnabled)
+                {
+                    if (Sounds.Sound.IsCorrectSound(5 - row, column))
+                        label.SetValue(Label.BackgroundProperty, GetGradient(Colors.Green));
+                    else
+                        label.SetValue(Label.BackgroundProperty, GetGradient(Colors.Red));
+                }
+                else
+                    label.SetValue(Label.BackgroundProperty, GetGradient(Colors.White));
+
+                label.BeginAnimation(UIElement.OpacityProperty, null);
+                label.Opacity = 0.5;
+
+                label.BeginAnimation(UIElement.OpacityProperty, animation);
+
+                btnRandomize.IsEnabled = true;
+            }
+        }
+
+        private void btnRandomize_Click(object sender, RoutedEventArgs e)
+        {
+            (sender as Button).IsEnabled = false;
+            RandomizeSound();
+        }
+
+        private void btnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            Sound.Play();
         }
     }
 }
